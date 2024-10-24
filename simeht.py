9b5500600d9e4599c649ab1b6853612b2710d0ff
Nguyen Thanh Quang <void.dev@outlook.com>
2024-10-24 16:51:11 +0700

    initial

---
+++ simeht.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+from sys import argv, exit
+from re import compile, RegexFlag
+from subprocess import run
+from glob import glob
+from os import path
+
+if len(argv) < 2:
+    exit("source file is not specified, quitting")
+
+p = compile("^//( ?)@(?P<attr>pattern|in|out|time|mem|bin) (?P<val>.+)$", RegexFlag.ASCII)
+
+class Constraints:
+    time: float = 1.0;
+    mem: int = 256;
+    pattern: str | None = None;
+    inp: str | None = None;
+    out: str | None = None;
+    bin: str | None = None;
+
+c = Constraints()
+
+with open(argv[1], "r") as f:
+    l = f.readline()
+    while l != "":
+        m = p.search(l)
+        if m == None:
+            break
+        mg = m.groupdict()
+        match mg["attr"]:
+            case "pattern":
+                c.pattern = str(mg["val"])
+            case "in":
+                c.inp = str(mg["val"])
+            case "out": c.out = str(mg["val"])
+            case "bin": c.bin = str(mg["val"])
+            case "time": c.time = float(mg["val"])
+            case "mem": c.mem = int(mg["val"])
+            case _: pass
+        l = f.readline()
+    print(c.time, c.mem, c.out, c.pattern, c.inp, c.bin)
+    if any(map(lambda x: x == None or x == "", [c.pattern, c.inp, c.out, c.bin])):
+        exit("missing one of 'pattern', 'in', 'out', 'bin'")
+
+paths = glob(str(c.pattern), recursive=False)
+
+for p in paths:
+    res = run(list(map(str, [
+        "isolate",
+        f"--dir=/wd={p}:norec",
+        f"--dir={path.dirname(str(c.bin))}:norec",
+        "-m", c.mem << 10,
+        "-i", path.join("/wd", str(c.inp)),
+        "-t", c.time,
+        "--run", c.bin
+    ])), text=True, capture_output=True)
+    with open(path.join(p, str(c.out)), "r") as eout:
+        for i, l in enumerate(res.stdout.splitlines()):
+            el = eout.readline().strip()
+            l = l.strip()
+            if el == "" and el != l:
+                exit(f"line {i}: expected eof, got '{l}'")
+            if el != l:
+                exit(f"line {i}: expected '{el}', got '{l}'")
+    print(f"{p} {res.stderr}")
+
+
+
